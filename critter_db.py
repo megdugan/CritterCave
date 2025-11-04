e1499685bdd6ac8b9922357f2cf471389481dda1
nina <ninahowley>
2025-11-04 15:35:49 -0500

    beginning of method signup

--- critter_db.py
+++ critter_db.py
@@ -1,4 +1,13 @@
 import cs304dbi as dbi
+from datetime import datetime
+
+def signup(conn, name, username, password): 
+    # hash the user password
+    created = datetime.now()
+    profilepic = "/default.png"
+    darkmode = False
+    curs = dbi.dict_cursor(conn)
+    # curs.execute( '''blah blah insert stuff''')
 
 # run main to test methods
 # remember to activate virtual environment: source ~/cs304/venv/bin/activate
