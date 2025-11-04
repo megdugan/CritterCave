42de2868b0376f54b793004d4bd7108e0ab95f7b
nina <ninahowley>
2025-11-04 15:32:32 -0500

    start of methods

---
+++ critter_db.py
@@ -0,0 +1,7 @@
+import cs304dbi as dbi
+
+# run main to test methods
+# remember to activate virtual environment: source ~/cs304/venv/bin/activate
+if __name__ == '__main__':
+    dbi.conf("crittercave_db")
+    conn = dbi.connect() # pass as conn argument for testing methods
