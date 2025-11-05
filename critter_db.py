7fe27d486c82879189cd724e865a43c34a6cf9b2
Ruth Perjuste <RuthPer>
2025-11-05 16:51:19 -0500

    added the functions for add_critter() ✅ get_critter_by_id()✅ delete_critter()✅ update_critter()

--- critter_db.py
+++ critter_db.py
@@ -36,8 +36,63 @@ def add_story(conn, cid: int, uid: int, story: str):
     #     where month(birthdate) = %s''',[month])
     return
 
+def add_critter(conn,uid,imagepath,name,desc):
+    """ This method will add a new critter to the database based on 
+    on the user entering the critters name and descritpion"""
+    time=datetime.now()
+    curs=dbi.dict_cursor(conn)
+    curs.execute("""
+            insert into critter(uid,imagepath,name,description,created)
+             values (%s,%s, %s,%s,%s) """,
+             [uid,imagepath,name,desc,time])
+    conn.commit()
+
+    #Then we get the gritter id for url redirection 
+    curs.execute('select last_insert_id()')
+    row = curs.fetchone()
+    return row
+
+def get_critter_by_id(conn,cid):
+    """
+    This method will grab critters by their critter id
+    """
+    curs=dbi.dict_cursor(conn)
+    curs.execute("""
+            select * from critter where cid=%s
+            """,[cid])
+    return curs.fetchone()
+
+def delete_critter(conn,cid):
+    """
+    This method will delete critters by their critter id
+    """
+    curs=dbi.dict_cursor(conn)
+    curs.execute("""
+            delete from critter where cid=%s
+            """,[cid])
+    conn.commit()
+
+def update_critter(conn,cid,imagepath,name,description):
+    """
+    This method will update critters by their critter id
+    """
+    curs=dbi.dict_cursor(conn)
+    curs.execute("""
+            update critter set imagepath=%s,name=%s,description=%s
+            where cid=%s
+            """,[imagepath,name,description,cid])
+    conn.commit()
+    
+
+
 # To test methods
 # Remember to activate virtual environment: source ~/cs304/venv/bin/activate
 if __name__ == '__main__':
     dbi.conf("crittercave_db")
     conn = dbi.connect() # pass as conn argument for testing methods
+    #critter=add_critter(conn,1,"path","Tommy","Tommy is a very bald critter")
+    #print(critter)
+    #critter_id=get_critter_by_id(conn,2)
+    #print(critter_id)
+    #delete_critter(conn,4)
+    update_critter(conn,3,imagepath="path",description="Wow",name="Sam")
