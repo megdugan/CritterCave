5014950761a01e62c64a24f97c53539b66c014ec
nina <ninahowley>
2025-11-12 16:47:46 -0500

    signup.html and route form complete

--- app.py
+++ app.py
@@ -34,8 +34,36 @@ def about():
     flash('this is a flashed message')
     return render_template('about.html', page_title='About Us')
 
-@app.route('/login/')
-def login():
+@app.route('/signup/', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        # if method is get, send a blank form
+        return render_template('signup.html', page_title='Sign Up')
+    else:
+        # if method is post, get contents of filled in form
+        conn = dbi.connect()
+        name = request.form.get('name')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print(name)
+        print(username)
+        print(password)
+        uid = profile.sign_up(conn, name, username, password)
+        # if duplicate key error, flash message
+        if uid == -2:
+            flash(f"Username [ {username} ] is taken. Please try again.")
+            return redirect(url_for('signup'))
+        # if a different error occured, flash message
+        if uid == -1:
+            flash("An error has occured.")
+            return redirect(url_for('signup'))
+        # if successful, redirect to user's page
+        user = profile.get_user_info(conn,uid)
+        critters = profile.get_critters_by_user(conn,uid)
+        return render_template('profile.html', user=user, critters=critters)
+
+@app.route('/signin/')
+def signin():
     return
 
 @app.route('/profile/<uid>')
